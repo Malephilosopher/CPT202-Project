5262055399de08dc99b7320db89c0faae62f12c8
Ivan <2350327610@qq.com>
2022-03-16 14:43:30 +0800

    2022.3.16 first commit

---
+++ Modao/src/main/java/com/xjtlu/cpt202/modao/ModaoApplication.java
@@ -0,0 +1,13 @@
+package com.xjtlu.cpt202.modao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModaoApplication.class, args);
+    }
+
+}
---
+++ Modao/src/test/java/com/xjtlu/cpt202/modao/ModaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjtlu.cpt202.modao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
