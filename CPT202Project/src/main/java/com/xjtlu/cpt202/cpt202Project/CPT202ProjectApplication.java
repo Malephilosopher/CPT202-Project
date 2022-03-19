7ab3da626bc960e9524933624f97a38b16699ccc
Ivan <2350327610@qq.com>
2022-03-19 22:09:42 +0800

    add application

---
+++ CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/CPT202ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xjtlu.cpt202.cpt202Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CPT202ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CPT202ProjectApplication.class, args);
+    }
+
+}
