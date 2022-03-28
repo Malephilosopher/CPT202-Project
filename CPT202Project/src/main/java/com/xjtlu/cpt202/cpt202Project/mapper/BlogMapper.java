b059bb73ca00ad6d2977aab923a03eafb5260abc
Naserien <94505962+Naserien@users.noreply.github.com>
2022-03-28 14:41:48 +0800

    Update BlogMapper.java

--- CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/mapper/BlogMapper.java
+++ CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/mapper/BlogMapper.java
@@ -7,40 +7,40 @@ import java.util.List;
 
 @Mapper
 public interface BlogMapper {
-    @Insert("insert into question(title,description,createid,tag,createtime) values (#{title},#{description},#{createid},#{tag},#{createtime})")
-    void createquestion(Blog Blog);
+    @Insert("insert into blog(title,description,createid,tag,createtime) values (#{title},#{description},#{createid},#{tag},#{createtime})")
+    void createblog(Blog Blog);
 
-    @Select("select * from question order by createtime desc limit #{offset},#{size} ")
+    @Select("select * from blog order by createtime desc limit #{offset},#{size} ")
     List<Blog> list(@Param("offset") int offset, @Param("size") int size);
 
-    @Select("select count(1) from question")
+    @Select("select count(1) from blog")
     int count();
 
-    @Select("select * from question where createid=#{userid} limit #{offset},#{size}")
+    @Select("select * from blog where createid=#{userid} limit #{offset},#{size}")
     List<Blog> listbyid(@Param("userid") int userid, @Param("offset") int offset, @Param("size") int size);
 
-    @Select("select count(1) from question where createid=#{userid}")
+    @Select("select count(1) from blog where createid=#{userid}")
     int countbyid(int userid);
 
-    @Select("select * from question where id=#{id}")
+    @Select("select * from blog where id=#{id}")
     Blog getbyId(int id);
 
-    @Update("update question set title=#{title},description=#{description},tag=#{tag},createtime=#{createtime} where id=#{id}")
+    @Update("update blog set title=#{title},description=#{description},tag=#{tag},createtime=#{createtime} where id=#{id}")
     void updatequestion(Blog Blog);
 
-    @Update("update question set view_count=view_count+1 where id=#{id}")
+    @Update("update blog set view_count=view_count+1 where id=#{id}")
     void updateView(int id);
 
-    @Update("update question set comment_count=comment_count+1 where id=#{parent_id}")
+    @Update("update blog set comment_count=comment_count+1 where id=#{parent_id}")
     void updatecomment(int parent_id);
 
-    @Select("select * from question where tag REGEXP #{result} and id!=#{id} limit 0,10")
+    @Select("select * from blog where tag REGEXP #{result} and id!=#{id} limit 0,10")
     List<Blog> getbytag(@Param("id") int id, @Param("result") String result);
 
-    @Select("select title from question where id=#{outerid}")
+    @Select("select title from blog where id=#{outerid}")
     String gettitlebyid(int outerid);
 
-    @Select("select * from question order by view_count desc limit 0,10")
+    @Select("select * from blog order by view_count desc limit 0,10")
     List<Blog> gettopten();
 }
 
