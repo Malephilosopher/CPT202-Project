a93c2fd50af543c1b4c1a02d8f487a3cb04d1be4
Jocelyn <Jocelynbao123@126.com>
2022-03-29 18:31:25 +0800

    Yijie.Bao 2022.3.29（增加了一些service， 加adduser和login）

--- CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/controller/CommentController.java
+++ CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/controller/CommentController.java
@@ -1,59 +1,59 @@
-//package com.xjtlu.cpt202.cpt202Project.controller;
-//import com.alibaba.fastjson.JSON;
-//import  com.xjtlu.cpt202.cpt202Project.entity.Comment;
-//
-//import com.xjtlu.cpt202.cpt202Project.service.CommentService;
-//import com.xjtlu.cpt202.cpt202Project.entity.Result;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//
-//@Controller
-//public class CommentController {
-//
-//    @Autowired
-//    private CommentService commentService;
-//    //展示所有的评论
-//    @GetMapping("/comments/{blogId}")
-//    public String listComments(@PathVariable("blogId") Long blogId, Model model) {
-//        List<Comment> comments = commentService.listCommentByBlogId(blogId);
-//        model.addAttribute("comments", comments);
-//        return "blog :: commentList";
-//    }
-//
-//
-//    /**
-//     * 添加一条评论
-//     * @param comment
-//     * @param userId
-//     * @param id
-//     * @return 添加成功：code:200, message:comment added successfully
-//     * @return 添加失败：code:300, message:comment add failed
-//     */
-//    @PostMapping("/comment")
-//    public String addComment(String comment, long userId, Long id) {
-//        Comment comm = JSON.parseObject(comment, Comment.class);
-//   //     Long blogId = addComment.getBlogId();
-//        comm.setAuthorId(userId);
-//        if("".equals(comm.getContent())){
-//            return JSON.toJSONString(Result.fail("Comment add failed"));
-//        }
-//        commentService.addComment(comm);
-//     //这两行看后期需要
-//    //    List<Comment> comments = commentService.listCommentByBlogId(blogId);
-//    //       model.addAttribute("comments", comments);
-//        return JSON.toJSONString(Result.success("Comment added successfully"));
-//    }
-//    //    删除评论
-//    @GetMapping("/comment/{blogId}/delete")
-//    public String delete(@PathVariable Long blogId,Comment comment,  Model model){
-//        commentService.deleteComment(comment);
-//        List<Comment> comments = commentService.listCommentByBlogId(blogId);
-//        model.addAttribute("comments", comments);
-//        return "blog";
-//    }
-//}
+package com.xjtlu.cpt202.cpt202Project.controller;
+import com.alibaba.fastjson.JSON;
+import  com.xjtlu.cpt202.cpt202Project.entity.Comment;
+
+import com.xjtlu.cpt202.cpt202Project.service.CommentService;
+import com.xjtlu.cpt202.cpt202Project.entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+    //展示所有的评论
+    @GetMapping("/comments/{blogId}")
+    public String listComments(@PathVariable("blogId") Long blogId, Model model) {
+        List<Comment> comments = commentService.listCommentByBlogId(blogId);
+        model.addAttribute("comments", comments);
+        return "blog :: commentList";
+    }
+
+
+    /**
+     * 添加一条评论
+     * @param comment
+     * @param userId
+     * @param id
+     * @return 添加成功：code:200, message:comment added successfully
+     * @return 添加失败：code:300, message:comment add failed
+     */
+    @PostMapping("/comment")
+    public String addComment(String comment, long userId, Long id) {
+        Comment comm = JSON.parseObject(comment, Comment.class);
+   //     Long blogId = addComment.getBlogId();
+        comm.setAuthorId(userId);
+        if("".equals(comm.getContent())){
+            return JSON.toJSONString(Result.fail("Comment add failed"));
+        }
+        commentService.addComment(comm);
+     //这两行看后期需要
+    //    List<Comment> comments = commentService.listCommentByBlogId(blogId);
+    //       model.addAttribute("comments", comments);
+        return JSON.toJSONString(Result.success("Comment added successfully"));
+    }
+    //    删除评论
+    @GetMapping("/comment/{blogId}/delete")
+    public String delete(@PathVariable Long blogId,Comment comment,  Model model){
+        commentService.deleteComment(comment);
+        List<Comment> comments = commentService.listCommentByBlogId(blogId);
+        model.addAttribute("comments", comments);
+        return "blog";
+    }
+}
--- CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/controller/PublishController.java
+++ CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/controller/PublishController.java
@@ -1,105 +1,105 @@
-//package com.xjtlu.cpt202.cpt202Project.controller;
-//
-//import com.xjtlu.cpt202.cpt202Project.cache.TagCache;
-//import com.xjtlu.cpt202.cpt202Project.dto.TagDto;
-//import com.xjtlu.cpt202.cpt202Project.entity.Blog;
-//import com.xjtlu.cpt202.cpt202Project.entity.User;
-//import com.xjtlu.cpt202.cpt202Project.mapper.BlogMapper;
-//import com.xjtlu.cpt202.cpt202Project.mapper.UserMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//import javax.annotation.Resource;
-//import javax.servlet.http.Cookie;
-//import javax.servlet.http.HttpServletRequest;
-//import java.util.List;
-//
-//
-////问题发布
-//@Controller
-//public class PublishController {
-//
-//    @Autowired
-//    private UserMapper userMapper;
-//    @Autowired
-//    private BlogMapper postMapper;
-//
-//    @GetMapping("/publish")
-//    public String publish(Model model) {
-//        //标签组
-//        TagCache tagCache=new TagCache();
-//        List<TagDto> tags = tagCache.gettags();
-//        model.addAttribute("tags",tags);
-//        return "publish";
-//    }
-//
-//    //发贴
-//    @PostMapping("/publish")
-//    public String publishpost(
-//            @RequestParam("title") String title,
-//            @RequestParam("content") String content,
-//            @RequestParam("tag") String tag,
-//            @RequestParam(value = "id", defaultValue = "-1") int id,
-//            HttpServletRequest request,
-//            Model model
-//    ) {
-//        model.addAttribute("title", title);
-//        model.addAttribute("content", content);
-//        model.addAttribute("tag", tag);
-//        //标签组
-//        TagCache tagCache = new TagCache();
-//        List<TagDto> tags = tagCache.gettags();
-//        model.addAttribute("tags", tags);
-//        //防止输入的问题为空
-//        if (title == null || title.equals("")) {
-//            model.addAttribute("error", "The title cannot be empty");
-//            return "publish";
-//        }
-//        if (content == null || content.equals("")) {
-//            model.addAttribute("error", "Content cannot be empty");
-//            return "publish";
-//        }
-//        //获取当前登陆用户的信息
-//        User user = null;
-//        Cookie[] cookies = request.getCookies();
-//        for (Cookie cookie : cookies) {
-//            if (cookie.getName().equals("token")) {
-//                String token = cookie.getValue();
-//                user = userMapper.findBytoken(token); //待定
-//            }
-//        }
-//        //将问题上传到数据库
-//        Blog post = new Blog();
-//        post.setTitle(title);
-//        post.setContent(content);
-//        post.setTag(tag);
-//        post.setAuthor_id(user.getId()); //待定
-//        post.set_post_time(System.currentTimeMillis());
-//        post.set_edit_time(System.currentTimeMillis());
-//        if (id == -1) {
-//            postMapper.createquestion(post);
-//        } else {
-//            post.setId(id);
-//            postMapper.updatequestion(post);
-//        }
-//        return "redirect:/index";
-//    }
-//
-//    @GetMapping("/publish/{id}")
-//    public String edit(@PathVariable(name = "id")int id,
-//                       Model model){
-//        Blog post= postMapper.getbyId(id);
-//        model.addAttribute("title", post.getTitle());
-//        model.addAttribute("description", post.getContent());
-//        model.addAttribute("tag", post.getTag());
-//        //用来标识问题是修改而不是重新创建
-//        model.addAttribute("id",post.getId());
-//        return "publish";
-//    }
-//}
+package com.xjtlu.cpt202.cpt202Project.controller;
+
+import com.xjtlu.cpt202.cpt202Project.cache.TagCache;
+import com.xjtlu.cpt202.cpt202Project.dto.TagDto;
+import com.xjtlu.cpt202.cpt202Project.entity.Blog;
+import com.xjtlu.cpt202.cpt202Project.entity.User;
+import com.xjtlu.cpt202.cpt202Project.mapper.BlogMapper;
+import com.xjtlu.cpt202.cpt202Project.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+
+//问题发布
+@Controller
+public class PublishController {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private BlogMapper postMapper;
+
+    @GetMapping("/publish")
+    public String publish(Model model) {
+        //标签组
+        TagCache tagCache=new TagCache();
+        List<TagDto> tags = tagCache.gettags();
+        model.addAttribute("tags",tags);
+        return "publish";
+    }
+
+    //发贴
+    @PostMapping("/publish")
+    public String publishpost(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("tag") String tag,
+            @RequestParam(value = "id", defaultValue = "-1") int id,
+            HttpServletRequest request,
+            Model model
+    ) {
+        model.addAttribute("title", title);
+        model.addAttribute("content", content);
+        model.addAttribute("tag", tag);
+        //标签组
+        TagCache tagCache = new TagCache();
+        List<TagDto> tags = tagCache.gettags();
+        model.addAttribute("tags", tags);
+        //防止输入的问题为空
+        if (title == null || title.equals("")) {
+            model.addAttribute("error", "The title cannot be empty");
+            return "publish";
+        }
+        if (content == null || content.equals("")) {
+            model.addAttribute("error", "Content cannot be empty");
+            return "publish";
+        }
+        //获取当前登陆用户的信息
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                user = userMapper.findBytoken(token); //待定
+            }
+        }
+        //将问题上传到数据库
+        Blog post = new Blog();
+        post.setTitle(title);
+        post.setContent(content);
+        post.setTag(tag);
+        post.setAuthor_id(user.getId()); //待定
+        post.set_post_time(System.currentTimeMillis());
+        post.set_edit_time(System.currentTimeMillis());
+        if (id == -1) {
+            postMapper.createquestion(post);
+        } else {
+            post.setId(id);
+            postMapper.updatequestion(post);
+        }
+        return "redirect:/index";
+    }
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id")int id,
+                       Model model){
+        Blog post= postMapper.getbyId(id);
+        model.addAttribute("title", post.getTitle());
+        model.addAttribute("description", post.getContent());
+        model.addAttribute("tag", post.getTag());
+        //用来标识问题是修改而不是重新创建
+        model.addAttribute("id",post.getId());
+        return "publish";
+    }
+}
 
