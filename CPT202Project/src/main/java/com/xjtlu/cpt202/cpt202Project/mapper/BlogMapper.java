e760c109482206d3ab850e1e780baa319f4af289
Naserien <94505962+Naserien@users.noreply.github.com>
2022-04-06 19:47:14 +0800

    Update BlogMapper.java
    更新注解

--- CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/mapper/BlogMapper.java
+++ CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/mapper/BlogMapper.java
@@ -7,15 +7,43 @@ import java.util.List;
 
 @Mapper
 public interface BlogMapper {
+    /**
+     * 根据用户id，限制的展示数量找到blog列表
+     * @param userId
+     * @param offset
+     * @param limit
+     * @return
+     */
     List<Blog> selectBlogs(int userId, int offset, int limit);
+
+    /**
+     * 根据用户id查找帖子
+     * @param userId
+     * @return
+     */
     //Param用于给参数起别名，并且在<if>里面使用，则必须起别名，动态拼凑条件时，只有一个参数
     int selectBlogRows(@Param("userId") int userId);
 
+    /**
+     * 插入帖子
+     * @param blog
+     * @return
+     */
     int insertBlog(Blog blog);
 
+    /**
+     * 根据帖子id查找帖子
+     * @param id
+     * @return
+     */
     Blog selectBlogById(int id);
 
-    int updateCommentCount(int id, int commentCount);
+    /**
+     * 删除帖子
+     * @param blog_id
+     * @return
+     */
+    int deleteBlog(int blog_id);
 }
 
 
