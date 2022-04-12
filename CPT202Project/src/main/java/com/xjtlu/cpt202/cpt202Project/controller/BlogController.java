1aa5729a05c2ea3b8ac80f698581fd105ccc0ece
Naserien <94505962+Naserien@users.noreply.github.com>
2022-04-12 15:26:45 +0800

    Update BlogController.java

--- CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/controller/BlogController.java
+++ CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/controller/BlogController.java
@@ -1,109 +1,60 @@
 package com.xjtlu.cpt202.cpt202Project.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.xjtlu.cpt202.cpt202Project.entity.Blog;
 import com.xjtlu.cpt202.cpt202Project.entity.Comment;
+import com.xjtlu.cpt202.cpt202Project.entity.Result;
 import com.xjtlu.cpt202.cpt202Project.entity.User;
-import com.xjtlu.cpt202.cpt202Project.service.Impl.BlogServiceImpl;
+import com.xjtlu.cpt202.cpt202Project.service.BlogService;
 import com.xjtlu.cpt202.cpt202Project.service.CommentService;
-import com.xjtlu.cpt202.cpt202Project.service.Impl.CommentServiceImpl;
-import com.xjtlu.cpt202.cpt202Project.service.Impl.UserServiceImpl;
 import com.xjtlu.cpt202.cpt202Project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-//帖子发布
+
 @RestController
 public class BlogController {
     @Autowired
-    private BlogServiceImpl blogService;
+    private BlogService blogService;
     @Autowired
-    private UserServiceImpl userService;
+    private UserService userService;
     @Autowired
-    private CommentServiceImpl commentService;
+    private CommentService commentService;
 
+    //帖子创作
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String addBlog(String title,String description, String content,String username, int like, int userid) {
-        User user = userService.getUser(userid);
+    public String addBlog(@RequestBody String information) {
+        Blog info = JSON.parseObject(information, Blog.class);
+        User user = userService.getUser(info.getAuthor_id());
         if (user == null) {
-            return "用户未登录";
+            return JSON.toJSONString(Result.create(300,"User not logged in"));
         }
         //帖子上传数据库
         Blog blog = new Blog();
-        blog.setId(user.getId());
-        blog.setTitle(title);
-        blog.setContent(content);
+        blog.setAuthor_id(user.getId());
+        blog.setTitle(info.getTitle());
+        blog.setDescription(info.getDescription());
+        blog.setContent(info.getContent());
         blog.setPost_time(System.currentTimeMillis());
         blogService.addBlog(blog);
 
-        // 报错的情况之后处理
-        return "发布成功";
+        // 无错返回
+        return JSON.toJSONString(Result.create(200,"create blog successfully"));
     }
 
+    //文章详情
     @RequestMapping(value = "/getPage", method = RequestMethod.GET)
-    public String BlogPage(@PathVariable("BlogId") int BlogId, Model model) {
-        // 帖子
-        Blog blog = blogService.findBlogById(BlogId);
-        model.addAttribute("blog", blog);
-        // 作者
-        User user = userService.getUser(blog.getAuthor_id());
-        model.addAttribute("user", user);
-
-//        // 评论分页信息
-//        page.setLimit(5);
-//        page.setPath("/discuss/detail/" + BlogId);
-//        page.setRows(blog.getComment_count());
-
-        // 评论: 给帖子的评论
-        // 回复: 给评论的评论
-        // 评论列表
-        List<Comment> commentList = commentService.listCommentByBlogId(blog.getId()); //long改int
-        // 评论VO列表
-        List<Map<String, Object>> commentVoList = new ArrayList<>();
-        if (commentList != null) {
-            for (Comment comment : commentList) {
-                // 评论VO
-                Map<String, Object> commentVo = new HashMap<>();
-                // 评论
-                commentVo.put("comment", comment);
-                // 作者
-                commentVo.put("user", userService.getUser(comment.getAuthorId())); //long改int
-
-                // 回复列表
-                List<Comment> replyList = commentService.listCommentByBlogId(comment.getId());
-                // 回复VO列表
-                List<Map<String, Object>> replyVoList = new ArrayList<>();
-                if (replyList != null) {
-                    for (Comment reply_comment : replyList) {
-                        Map<String, Object> replyVo = new HashMap<>();
-                        // 回复
-                        replyVo.put("reply_comment", reply_comment);
-                        // 作者
-                        replyVo.put("user", userService.getUser(reply_comment.getAuthorId())); //long改int
-                        // 回复目标
-                        User target = reply_comment.getParentCommentId() == 0 ? null : userService.getUser(reply_comment.getParentCommentId());
-                        replyVo.put("target", target);
-
-                        replyVoList.add(replyVo);
-                    }
-                }
-                commentVo.put("replys", replyVoList);
-
-                commentVoList.add(commentVo);
-            }
+    public String BlogPage(@RequestParam(value = "id") int id) {
+        if (id == 0) {
+            return JSON.toJSONString(Result.create(300,"Can't find blog"));
         }
-
-        model.addAttribute("comments", commentVoList);
-
-        return "get success";
+        // 帖子信息
+        Blog blog = blogService.findBlogById(id);
+        // 作者信息
+        User user = userService.getUser(blog.getAuthor_id());
+        return JSON.toJSONString(Result.create(200,"Get Blog and author information success", blog))+
+        JSON.toJSONString(Result.create(200,"Get author information success", blog));
     }
 }
 
