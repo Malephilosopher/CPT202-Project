3323cd11a3aaee6c54b28f5b71e2791319d656b4
Naserien <94505962+Naserien@users.noreply.github.com>
2022-04-21 14:45:31 +0800

    Update BlogService.java

--- CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/service/BlogService.java
+++ CPT202Project/src/main/java/com/xjtlu/cpt202/cpt202Project/service/BlogService.java
@@ -8,15 +8,18 @@ import java.util.List;
 
 
 public interface BlogService {
+    //get a time-sorted blog_list based on the limit amount
+    List<Blog> findBlogs(int limit);
 
-    public List<Blog> findBlogs(int limit);
+    //add blog info into database
+    int addBlog(Blog blog);
 
+    //find blog by blog_id
+    Blog findBlogById(int id);
 
+    //delete blog by blog_id
+    int deleteBlog(int blog_id);
 
-    public  int addBlog(Blog blog);
-
-    public  Blog findBlogById(int id);
-
-    public int deleteBlog(int blog_id);
+    //get a time-sorted blog_list based on the keyword
     List<Blog> searchBlogs(String keyword);
 }
